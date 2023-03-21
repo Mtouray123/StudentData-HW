fa20e4ecd69a31fef70795eb181170f902c6851a
Mahamadou Touray <mahamadou12@gmail.com>
2023-03-21 18:12:45 -0400

    StudenData

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { useState } from 'react';
+import { StudentData } from './Components/StudentData';
+import Student from './Components/Student';
+
+function App() {
+  const [data, setData] = useState(StudentData)
+  
+  return (
+    <div className="App">
+      {data.map(student => <Student info={student} />) }
+
+    </div>
+  );
+}
+
+export default App;
